f43f31488c54238a2d630e1e92181d2d0183ce4a
Julio Campuzano <campuzano.j@gmail.com>
2012-10-30 14:13:58 +0100

    Modificación del CSS para las pantallas con listados

--- EditorPreguntas/apps/frontend/modules/dificultad/templates/indexSuccess.php
+++ EditorPreguntas/apps/frontend/modules/dificultad/templates/indexSuccess.php
@@ -1,19 +1,20 @@
-<h1>Ecdl dificultads List</h1>
+<div class="editar">
+    <h2>Lista de dificultades disponibles</h2>
 
-<table>
-  <thead>
-    <tr>
-      <th>Nombre</th>
-      <th>Valor</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($ecdl_dificultads as $ecdl_dificultad): ?>
-    <tr>
-      <td><a href="<?php echo url_for('dificultad_edit', $ecdl_dificultad) ?>"><?php echo $ecdl_dificultad->getNombre() ?></a></td>
-      <td><?php echo $ecdl_dificultad->getValor() ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
- 
+    <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Valor</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($ecdl_dificultads as $ecdl_dificultad): ?>
+                <tr>
+                    <td><a href="<?php echo url_for('dificultad_edit', $ecdl_dificultad) ?>"><?php echo $ecdl_dificultad->getNombre() ?></a></td>
+                    <td><?php echo $ecdl_dificultad->getValor() ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
--- EditorPreguntas/apps/frontend/modules/modulo/templates/indexSuccess.php
+++ EditorPreguntas/apps/frontend/modules/modulo/templates/indexSuccess.php
@@ -1,20 +1,21 @@
 <?php slot('title', 'Edición de los módulos del ECDL') ?>
+<div class="editar">
+    <h2>Ecdl modulos List</h2>
 
-<h2>Ecdl modulos List</h2>
-
-<table>
-  <thead>
-    <tr>
-      <th>Nombre</th>
-      <th>Descripcion</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($ecdl_modulos as $ecdl_modulo): ?>
-    <tr>
-      <td><a href="<?php echo url_for('modulo_edit', $ecdl_modulo) ?>"><?php echo $ecdl_modulo->getNombre() ?></a></td>
-      <td><?php echo $ecdl_modulo->getDescripcion() ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+    <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Descripcion</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($ecdl_modulos as $ecdl_modulo): ?>
+                <tr>
+                    <td><a href="<?php echo url_for('modulo_edit', $ecdl_modulo) ?>"><?php echo $ecdl_modulo->getNombre() ?></a></td>
+                    <td><?php echo $ecdl_modulo->getDescripcion() ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
